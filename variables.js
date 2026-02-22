a3364e4a76e928af411d13e0b28833073db5bd15
mdsulemanr <mdsulemanr@gmail.com>
2026-02-22 12:59:01 +0500

    practice

---
+++ variables.js
@@ -0,0 +1,22 @@
+let fname = 'arisha';
+let lname = 'suleman';
+let fullName = fname + ' ' + lname;
+let age = 12;
+let x = 5;
+let y = 89;
+let z = 830;
+
+let price = 30;
+price = 100;
+const pi = 3.14;
+pi = 55;
+console.log(pi);
+console.log(price);
+let mySelf = 'My name is ' + fullName + ' and I am ' + age.toString() + ' years old.';
+console.log(fullName);
+console.log(age);
+console.log(fname);
+console.log(x+y-z-y*z*x);
+console.log('x+y-z-y*z*x');
+console.log(mySelf);
+
